4a3f8fec892829ba7f7bdf3ce8b7aaa7af95914c
Luiza Gomes <luizagomes1149@gmail.com>
2024-07-16 11:29:05 -0400

    Initial Commit

---
+++ PositivePlace/PositivePlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositivePlaceApp.swift
+//  PositivePlace
+//
+//  Created by Scholar on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PositivePlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
