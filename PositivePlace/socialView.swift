688e70c5fedf6654d0ec17ec04d318b264fe0bec
Luiza Gomes <luizagomes1149@gmail.com>
2024-07-17 15:39:33 -0400

    I finished the skeleton of the socialView.

--- PositivePlace/socialView.swift
+++ PositivePlace/socialView.swift
@@ -15,17 +15,55 @@ struct socialView: View {
                 .ignoresSafeArea()
             
             VStack {
+                Spacer(minLength: 80)
+                
                 Text("Social Media")
-                    .font(.title)
+                    .font(.largeTitle)
                     .foregroundColor(Color("royalBlue"))
-                
-                
-                Text("Social Media")
-            .font(.title2)
+                Spacer(minLength: 200)
+
+                Text("YouTube")
+                    .font(.title)
             .foregroundColor(.ivory)
             .frame(width: 275.0, height: 50.0)
             .background(.powderBlue)
             .cornerRadius(100.0)
+                Spacer()
+                
+                Text("Pintrest")
+                    .font(.title)
+            .foregroundColor(.ivory)
+            .frame(width: 275.0, height: 50.0)
+            .background(.pastelMagenta)
+            .cornerRadius(100.0)
+            Spacer()
+                
+                Text("Instagram")
+                    .font(.title)
+            .foregroundColor(.ivory)
+            .frame(width: 275.0, height: 50.0)
+            .background(.champagnePink)
+            .cornerRadius(100.0)
+                Spacer()
+                
+                Text("Spotify")
+                    .font(.title)
+            .foregroundColor(.ivory)
+            .frame(width: 275.0, height: 50.0)
+            .background(.emerald)
+            .cornerRadius(100.0)
+                Spacer()
+                
+                Text("Tik-Tok")
+                    .font(.title)
+            .foregroundColor(.ivory)
+            .frame(width: 275.0, height: 50.0)
+            .background(.tropicalBlue)
+            .cornerRadius(100.0)
+                Spacer()
+                
+                
+                Spacer(minLength: 130)
             }
         }
         
